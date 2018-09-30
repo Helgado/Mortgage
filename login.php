a00fc0c4f843eb6e37c9632aec7f9bc51bf89bdd
Helgado <Harley.Delgado001@umb.edu>
2018-09-30 14:25:52 -0400

    Add files via upload
    Put up html/php for a log in page.  Doesn't actually do anything yet.  Changed button on welcome.html to link there.

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Log In Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Mortgage Central</h1>
+  <p>This page is non-operational rn</p>
+</div>
+
+<div class="container">
+    <form action="login.php" method="post">
+		<div class="form-group">
+			<label for="email">Email address:</label>
+			<input type="email" class="form-control" id="email">
+		</div>
+		<div class="form-group">
+			<label for="pwd">Password:</label>
+			<input type="password" class="form-control" id="pwd">
+		</div>
+		<div class="btn-group">
+			<button type="submit" name="login" class="btn btn-primary">Log In</button>
+			<button type="submit" name="signup" class="btn btn-primary">Sign Up</button>
+		</div> 
+	</form>
+</div>
+
+</body>
+</html>
